fd55f59904735896edccb46383b8b89947a6dab9
SwardaMashere <103556100+SwardaMashere@users.noreply.github.com>
2025-10-25 20:50:38 +0530

    stack and heap

--- 01_basics/datatype_conversion.js
+++ 01_basics/datatype_conversion.js
@@ -1,4 +1,25 @@
-let somenumber=42;
-let stringnumber=String(somenumber);
-console.log(typeof stringnumber); // "string"
-console.log(stringnumber); // "42"
+// let somenumber=42;
+// let stringnumber=String(somenumber);
+// console.log(typeof stringnumber); // "string"
+// console.log(stringnumber); // "42"
+
+
+/*+++++++++++++++++++++++++++++Memory types++++++++++++++++++++++++++++++++++++++++*/
+//Stack Heap Memory
+//Stack(primitive) copy of variable
+//Heap(non-primitive)  reference of varibale so that it can change
+
+// let firstname="Swarda";
+// let anothername="Mashere" //copy of variable in stack memory
+// anothername="swarda mashere"
+// console.log(firstname);
+// console.log(anothername);
+/*+++++++++++++++++++++++++++++Memory types++++++++++++++++++++++++++++++++++++++++*/
+
+let userOne={
+    name:"Swarda",
+    age:22
+}
+let userTwo=userOne; //reference of variable in heap memory i.e original value
+console.log(userOne);
+console.log(userTwo);
