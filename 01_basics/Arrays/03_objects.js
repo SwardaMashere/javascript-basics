d45382763a4607b2573dd735407efb33a5c288c9
SwardaMashere <103556100+SwardaMashere@users.noreply.github.com>
2025-11-13 12:09:34 +0530

    objects_basics

---
+++ 01_basics/Arrays/03_objects.js
@@ -0,0 +1,32 @@
+//Singleton- when the object is created by constructor
+//ject.create;
+
+//declare symbol
+const mySym=Symbol("Key1");
+
+//object literals
+const JsUser={
+    name:"Swarda",
+    age:22,
+    [mySym]:"Swarda", //define a symbol like this[]
+    hobbies:["reading","coding","travelling"],
+    email:"swardamashere1439@gmail.com"
+}
+//accesss objects
+// console.log(JsUser.email);  //.operator to access object values
+// console.log(JsUser["hobbies"]) 
+// console.log(JsUser["email"]);
+
+//access Symbol
+// console.log (JsUser[mySym]); //string
+// console.log(JsUser); // [Symbol(Key1)]: 'Swarda'
+
+//functions
+JsUser.greeting=function(){
+    console.log("Hello Swarda");
+}
+JsUser.greeting2=function(){
+    console.log("Hello "
+}
+console.log(JsUser.greeting);
+console.log(JsUser.greeting2);
